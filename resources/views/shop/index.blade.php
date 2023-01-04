7bed84df70fe109592f5a9469c2914a481e444a3
Rick <eralon18@gmail.com>
2023-01-04 17:10:14 +0100

    .

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,20 +5,22 @@ welcome to the shop
 
 <h2>Categories</h2>
 
-    <img src="{{ url('storage/photos/Category.jpg') }}">
+
         
         
 <ul>
 @foreach ($categories as $category)
 <li>
+<div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 <label for="photo" class="block text-sm font-medium text-gray-700"> Background </label>
     <div class="mt-1 sm:mt-0 sm:col-span-2">
         <div class ="flex items-center"> 
-            <span class="h-50 w-50 rounded-full overflow-hidden bg-gray-100">
-                <img class="h-full object-cover" src="{{ url('storage/'.$category->photo) }}">         
+            <span class="h-15 w-15 square-full overflow-hidden bg-gray-100">
+                <a href="#"><img class="h-full object-cover" src="{{ url('storage/'.$category->photo) }}" width='300px' height='200px' ></a>
             </span>
         </div>
     </div>
+</div>
 </li>
 @endforeach
 </ul>
