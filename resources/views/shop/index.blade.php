2bbe8ad4e0728e80bbd287fbbb1be7b39e17620a
Rick <eralon18@gmail.com>
2023-01-05 20:35:47 +0100

    .

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -30,13 +30,42 @@ welcome to the shop
 @endforeach
 </ul>
 <!-- features -->
-<ul>
+<h1>Show featured/sale?</h1>
+
+<ul class="grid grid-cols-6 gap-4"> 
+@foreach ($category->products as $product)
+<li>
+<div class="">
+
+    <div class="">
+        <div class =""> 
+            <span class="">
+                <a href="{{route('products.show', $product->id)}}"><img class="h-full object-cover" src="{{ url('storage/'.$product->photo) }}" width='300px' height='200px' ></a>
+            </span>
+            <div class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+            Product: {{$product->name}}<br>
+            Price ={{$product->price}} <br>
+
+            <input type='number' id="quantity" placeholder='1'></input><button type ='submit' class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">add to cart</button>
+</div>
+        </div>
+
+    </div>
+</div>
+</li>
+@endforeach
+</ul>
+<br>
+
+<h1>Show alles </h1>
+<ul class="grid grid-cols-6 gap-4">
+@foreach ($categories as $category)
 @foreach ($category->products as $product)
 <li>
 <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 
     <div class="mt-1 sm:mt-0 sm:col-span-2">
-        <div class ="flex items-center"> 
+        <div class =""> 
             <span class="h-15 w-15 square-full overflow-hidden bg-gray-100">
                 <a href="{{route('products.show', $product->id)}}"><img class="h-full object-cover" src="{{ url('storage/'.$product->photo) }}" width='300px' height='200px' ></a>
             </span>
@@ -51,6 +80,7 @@ welcome to the shop
     </div>
 </div>
 </li>
+@endforeach
 @endforeach
 </ul>
 
