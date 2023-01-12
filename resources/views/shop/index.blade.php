90ed7703ed858f2f9302bf65a27b588a0cef1ffe
Rick <eralon18@gmail.com>
2023-01-12 17:02:48 +0100

    .

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -8,14 +8,16 @@ welcome to the shop
 
 <h2>Categories</h2>
 
+Pexels voor pictures
 
         
         
-<ul>
+        
+<ul class="grid grid-cols-6 gap-4">
 @foreach ($categories as $category)
 <li>
 <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-<label for="photo" class="block text-sm font-medium text-gray-700"> Category </label>
+<label>{{$category->id}} </label>
     <div class="mt-1 sm:mt-0 sm:col-span-2">
         <div class ="flex items-center"> 
             <span class="h-15 w-15 square-full overflow-hidden bg-gray-100">
