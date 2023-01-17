fa7ec7ac68baf622bec5f06c1f37c2b533bb1d9a
Rick <eralon18@gmail.com>
2023-01-17 01:52:32 +0100

    checkout
    initial migrations setup

---
+++ database/migrations/2023_01_17_005049_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreinId('product_id');
+            $table->text('name');
+            $table->text('price');
+            $table->text('quantity');
+            $table->text('subtotal_price');
+            $table->text('order_id');
+/*            $table->foreignId('');
+            $table->unsignedBigInteger('order_id'); */
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
