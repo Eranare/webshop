f3602a5f138befd6c1cd5069459b99a6bc4f244e
MadFalc <68642371+MadFalc@users.noreply.github.com>
2023-01-20 14:16:31 +0100

    Veranderingen
    pendingorders pagina's

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Order;
 
+use \Illuminate\Support\Facades\Auth;
 class OrderController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       if (Auth::check()) {
+        $pendingorders = Order::all();
+
+        return view('admin.pending.adminpending')->with('pendingorders',$pendingorders); 
+       }
+       else return view('auth.login');
+    }
+
+        /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $order = Order::findOrFail($id);
+
+        return view('admin.pending.showpending', compact('order'));
+    }
 }
+
---
+++ resources/views/admin/Pending/showpending.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h1 class="text-xl"> Show pending order</h1>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <a class="bg-blue-200 px-3 py-2 m-8 rounded shadow-md" href="{{ route('adminpending.index') }}"> Back</a>
+        </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>id:</strong>
+                {{ $order->id }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>customer_id:</strong>
+                {{ $order->customer_id }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>cart:</strong>
+                {{ $order->cart }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>price:</strong>
+                {{ $order->price }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>created_at:</strong>
+                {{ $order->created_at }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>updated_at:</strong>
+                {{ $order->updated_at }}
+            </div>
+        </div>
+    </div>
+@endsection
