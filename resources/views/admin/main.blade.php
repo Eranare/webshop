6bea5615ec62bb4f5c990739bb96794fffde0cb2
Rick <eralon18@gmail.com>
2023-01-30 14:27:18 +0100

    23131

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+<div class= "container">
               <div class="text-2xl">Welcome to the admin panel</div>
-              <video id='video'autoplay="true" muted="true" loop="true" class='fixed  pointer-events-none -z-20 w-10'  src = "{{asset('/public/background/spooder.mp4')}}"></video>
-
+              <video id='video'autoplay="true" muted="true" loop="true" class='fixed  pointer-events-none  w-50%'  src = "{{asset('spooder.mp4')}}"></video>
+</div>
 @endsection
