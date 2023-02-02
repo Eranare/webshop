1f3ebed2ebc4cd6ce02ec51f5537e7fe2fac4fb5
MadFalc <68642371+MadFalc@users.noreply.github.com>
2023-02-02 16:26:19 +0100

    vegan notitie upgrade

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -32,7 +32,7 @@
                 <img class="h-full object-cover" src="{{ url('storage/'.$product->photo) }}" width='300px' height='200px' >
                 </span>
                 
-                <div class="flex justify-center items-center h-10 bg-slate-50 place-items-center border">
+                <div class="inline-block justify-center items-center max-h-10 bg-slate-50 opacity-75 place-items-center border">
                 @if (str_contains(strtolower($product->ingredients), 'peanut'))
                     <img class="w-10 inline-block" src="{{ asset('images/peanut-amber_109463.png') }}" title="peanut allergy" alt="peanut allergy">
                 @elseif  (str_contains(strtolower($product->ingredients), 'nut'))
