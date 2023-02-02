092d5f848c5dd2e159d83c93084afb9cae1156f9
Rick <eralon18@gmail.com>
2023-02-02 18:51:58 +0100

    Update payment.blade.php

--- resources/views/checkout/payment.blade.php
+++ resources/views/checkout/payment.blade.php
@@ -2,8 +2,31 @@
 Show actual number
 
 push button sends to confirm
--->
-<img class = "w-full h-full -z-5" src="{{asset('bevestig.jpg')}}"  > 
+--><!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <script src ="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+<img class = "w-full h-full -z-5 flex" src="{{asset('bevestig.jpg')}}"  > 
 ${{ Cart::getTotal() }}
 <br>
 <style>
@@ -16,3 +39,4 @@ ${{ Cart::getTotal() }}
 <form action="{{ route('cart.postCheckout') }}" method="GET" enctype="multipart/form-data">
 <button> confirm </button>
 </form>
+</body>
